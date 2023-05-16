325c0b12333d378e2d944b33b1181217c189fe69
Henrique Andrade <henrique.ccda@gmail.com>
2023-05-16 15:38:57 -0300

    Roteamento de URL.

---
+++ dundie/routes/user.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter
+from sqlmodel import Session, select
+from dundie.models.user import User, UserResponse
+from dundie.db import ActiveSession
+
+
+router = APIRouter()
+
+
+@router.get("/", response_model=list[UserResponse])
+async def list_users(*, session: Session = ActiveSession):
+    """List all users from database"""
+    users = session.exec(select(User)).all()
+    return users
