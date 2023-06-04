67f68fd1d2810fd136f765719d998db57d63cbc9
Henrique Andrade <henrique.ccda@gmail.com>
2023-06-04 18:06:38 -0300

    Criando Filtros, Ordenação e Paginação.

--- dundie/routes/transaction.py
+++ dundie/routes/transaction.py
@@ -1,9 +1,15 @@
-from fastapi import APIRouter, Body, HTTPException
+from typing import Optional
+from fastapi import APIRouter, Body, Depends, HTTPException
 from dundie.auth import AuthenticateUser
 from dundie.db import ActiveSession
-from dundie.models import User
+from dundie.models import User, Transaction
+from dundie.models.serializers import TransactionResponse
 from dundie.tasks.transaction import TransactionError, add_tranaction, Transaction
-from sqlmodel import select, Session
+from sqlmodel import select, Session, text
+from fastapi_pagination import Page, Params
+from fastapi_pagination.ext.sqlmodel import paginate
+from sqlalchemy.orm import aliased
+
 
 router = APIRouter()
 
@@ -28,3 +34,43 @@ async def create_transaction(
 
     # At this point there was no error, so we can return
     return {"message": "Transaction added"}
+
+
+@router.get("/", response_model=Page[TransactionResponse])
+async def list_transactions(
+    *,
+    current_user: User = AuthenticateUser,
+    session: Session = ActiveSession,
+    params: Params = Depends(),
+    user: Optional[str] = None,
+    from_user: Optional[str] = None,
+    order_by: Optional[str] = None,  #order_by=date, value -date,-value
+):
+    """List all transactions"""
+    query =  select(Transaction)
+
+    # filtros opcionais AND
+    if user:
+        query = query.join(
+            User, Transaction.user_id == User.id
+        ).where(User.username == user)
+
+    if from_user:
+        FromUser = aliased(User)
+        query = query.join(
+            FromUser, Transaction.from_id == FromUser.id
+        ).where(FromUser.username == from_user)
+
+    # access filters
+    if not current_user.superuser:
+        query = query.where(
+            (Transaction.user_id == current_user.id) | (Transaction.from_id == current_user.id)
+        )
+
+    if order_by:
+        order_text = text(
+            order_by.replace("-", "") + " " + ("desc" if "-" in order_by else "asc")
+        )
+        query = query.order_by(order_text)
+
+    return paginate(query=query, session=session, params=params)
